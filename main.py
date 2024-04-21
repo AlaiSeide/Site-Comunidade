98c402fb22580ccfa252f5129827c4bf7437d874
Alai Seide <154024184+AlaiSeide@users.noreply.github.com>
2024-04-21 19:53:27 +0200

    validacao dos formularios alertas de sucesso e redirecionamento

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import FormCriarConta, FormLogin
 
 
@@ -26,15 +26,31 @@ def usuarios():
 
 
 # pagina de login e criar_conta
-@app.route('/login')
+# A funcao e uma pagina de formulario tem que ter o metodo POST/GET
+@app.route('/login', methods=['GET', 'POST'])
 def login():
 
-    # instanciando o meu formulario de login
+    # instanciando o meu formulario de login a minha classe FormLogin()
     form_login = FormLogin()
 
-
-    # instanciando o meu formulario de criar_conta
+    # instanciando o meu formulario de criar_conta a minha classe FormCriarConta()
     form_criarconta = FormCriarConta()
+
+    # Verifica se o usuario fez login com sucesso
+    if form_login.validate_on_submit() and 'botao_submit_login' in request.form():
+        # exibir msg de login feito com sucesso aseguido do e-amil dessa pessoal
+        flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
+        # redirecionar para outra pagina
+        # o return deve estar sempre atras do redirect(url_for('home'))
+        return redirect(url_for('home'))
+
+    # Verifica se o usuario criou conta com sucesso
+    if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form():
+        # exibir msg de conta criada com sucesso
+        flash(f'Conta Criada para o e-mail: {form_criarconta.email.data}', 'alert-success')
+        # redirecionar para outra pagina
+        # o return deve estar sempre atras do redirect(url_for('home'))
+        return redirect(url_for('home'))
 
     # form_login=form_login, form_criarconta=form_criarconta está dentro da minha funcao render_template() para poderem ser mostrados dentro da minha pagina html
     return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
