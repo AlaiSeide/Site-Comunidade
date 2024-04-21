928e98d48a696df3c7892fafd86daa10642be8b3
Alai Seide <154024184+AlaiSeide@users.noreply.github.com>
2024-04-21 18:42:15 +0200

    personalizei o meu formularios e mostrei os campos na minha pagina html

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for
+from forms import FormCriarConta, FormLogin
 
 
 app = Flask(__name__)
@@ -20,14 +21,23 @@ def contato():
 # pagina de usuarios
 @app.route("/usuarios")
 def usuarios():
-    # meus_usuarios=lista_usuarios está dentro da minha funcao render_template() para poder ser mostrado dentro da minha pagina html
+    # meus_usuarios=lista_usuarios está dentro da minha funcao render_template() para poderem ser mostrados dentro da minha pagina html
     return render_template('usuarios.html', meus_usuarios=lista_usuarios)
 
 
 # pagina de login e criar_conta
 @app.route('/login')
 def login():
-    return render_template('login.html')
+
+    # instanciando o meu formulario de login
+    form_login = FormLogin()
+
+
+    # instanciando o meu formulario de criar_conta
+    form_criarconta = FormCriarConta()
+
+    # form_login=form_login, form_criarconta=form_criarconta está dentro da minha funcao render_template() para poderem ser mostrados dentro da minha pagina html
+    return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
 
 if __name__ == '__main__':
     app.run(debug=True)
