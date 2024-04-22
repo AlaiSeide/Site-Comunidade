49e1201963f19b810ff260f7579cc3c127f52a15
Alai Seide <154024184+AlaiSeide@users.noreply.github.com>
2024-04-22 21:08:12 +0200

    requisicoes no meu banco de dados

---
+++ teste2.py
@@ -0,0 +1,24 @@
+
+# Claro! O with em Python é uma maneira elegante de lidar com recursos que precisam ser abertos e fechados de forma adequada, como arquivos, conexões de banco de dados ou sockets de rede. Ele garante que os recursos sejam fechados corretamente, mesmo se ocorrerem erros durante o processamento.
+
+# Aqui está uma explicação super simples:
+
+# Imagine que você está lendo um livro em uma biblioteca. Você pega o livro emprestado (abre o recurso) e, quando termina de ler, você o devolve (fecha o recurso). Usando o with em Python, é como se você dissesse: "Por favor, abra este livro para mim, e quando eu terminar de ler, devolva-o automaticamente, mesmo se eu esquecer".
+
+# Vamos ver um exemplo prático com a leitura de um arquivo:
+
+
+
+
+# Sem o with
+file = open('arquivo.txt', 'r')
+conteudo = file.read()
+print(conteudo)
+file.close()  # Precisamos lembrar de fechar o arquivo
+
+# Com o with
+with open('arquivo.txt', 'r') as file:
+    conteudo = file.read()
+    print(conteudo)
+
+# Aqui, o arquivo já está fechado automaticamente, não precisamos nos preocupar com isso
