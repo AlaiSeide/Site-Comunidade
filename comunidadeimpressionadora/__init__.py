f8d1f231d32f69e557ab9c46b711abbddafb9209
Alai Seide <154024184+AlaiSeide@users.noreply.github.com>
2024-05-05 22:11:49 +0200

    Criei variaveis de ambiente para fazer conexao com o banco de dados Postgre

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -2,11 +2,22 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
+
 
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '0842ad099743ac670a2b8a9ff48f7c31'
+# localhost de Bötelkamp
+localhost =  '192.168.56.1'
+
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL"):
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://root:flashreverso20@{localhost}/comunidade'
+
+
 # Configuração da URI do banco de dados SQLite
 # Indica ao SQLAlchemy onde encontrar o banco de dados
 # 'sqlite:///comunidade.db' significa que o banco de dados SQLite está localizado no arquivo 'comunidade.db' no diretório atual
@@ -17,10 +28,10 @@ app.config['SECRET_KEY'] = '0842ad099743ac670a2b8a9ff48f7c31'
 # localhost =  '192.168.220.130'
 
 # localhost de Bötelkamp
-localhost =  '192.168.56.1'
+#localhost =  '192.168.56.1'
 
 # Configuração da conexão com o banco de dados MySQL no XAMPP
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://root:flashreverso20@{localhost}/comunidade'
+#app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://root:flashreverso20@{localhost}/comunidade'
 
 # Criação de um objeto SQLAlchemy e associação à instância do Flask (app)
 # SQLAlchemy é uma biblioteca para trabalhar com bancos de dados relacionais de forma orientada a objetos
